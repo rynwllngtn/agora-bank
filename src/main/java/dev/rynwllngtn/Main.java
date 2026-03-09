fb3f78dbfd1d7a6787102178df15f3872d23480d
Ryan Wellington <rynwllngtn@gmail.com>
2026-03-09 19:33:06 -0300

    feat: Primeiro update no banco de dados

--- src/main/java/dev/rynwllngtn/Main.java
+++ src/main/java/dev/rynwllngtn/Main.java
@@ -3,13 +3,11 @@ package dev.rynwllngtn;
 import dev.rynwllngtn.db.Db;
 import dev.rynwllngtn.db.DbException;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main {
-
-    static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     static void main() {
 
@@ -19,30 +17,18 @@ public class Main {
         try {
             connection = Db.getConnection();
             preparedStatement = connection.prepareStatement(
-                    "INSERT INTO seller" +
-                        "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
-                        "VALUES" +
-                        "(?, ?, ?, ?, ?)",
-                        Statement.RETURN_GENERATED_KEYS);
+                    "UPDATE seller " +
+                        "SET BaseSalary = BaseSalary + ? " +
+                        "WHERE DepartmentId = ? ");
 
-            preparedStatement.setString(1, "Username");
-            preparedStatement.setString(2, "user@email.com");
-            preparedStatement.setDate(3, new java.sql.Date(dateFormat.parse("07/01/2002").getTime()));
-            preparedStatement.setDouble(4, 5000);
-            preparedStatement.setInt(5, 1);
+            preparedStatement.setDouble(1, 500);
+            preparedStatement.setInt(2, 1);
 
             int rows = preparedStatement.executeUpdate();
-            if (rows > 0) {
-                ResultSet resultSet = preparedStatement.getGeneratedKeys();
-                while (resultSet.next()) {
-                    int id = resultSet.getInt(1);
-                    IO.println("Novo item com id " + id);
-                }
-            }
-        } catch (SQLException e) {
+            IO.println(rows + " linhas alteradas!");
+        }
+        catch (SQLException e) {
             throw new DbException(e.getMessage());
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
         finally {
             Db.closeStatement(preparedStatement);
