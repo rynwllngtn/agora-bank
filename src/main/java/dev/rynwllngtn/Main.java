b050838f0f47d427c758d58d2898e54162a0479b
Ryan Wellington <rynwllngtn@gmail.com>
2026-03-09 19:21:13 -0300

    feat: Primeiro insert no banco de dados

--- src/main/java/dev/rynwllngtn/Main.java
+++ src/main/java/dev/rynwllngtn/Main.java
@@ -1,34 +1,51 @@
 package dev.rynwllngtn;
 
 import dev.rynwllngtn.db.Db;
+import dev.rynwllngtn.db.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
 
+    static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
     static void main() {
 
-        Statement statement = null;
-        ResultSet resultSet = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
 
         try {
-            Connection connection = Db.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM department");
+            connection = Db.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO seller" +
+                        "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
+                        "VALUES" +
+                        "(?, ?, ?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS);
 
-            while (resultSet.next()) {
-                IO.println(resultSet.getInt("Id") + "," + resultSet.getString("Name"));
+            preparedStatement.setString(1, "Username");
+            preparedStatement.setString(2, "user@email.com");
+            preparedStatement.setDate(3, new java.sql.Date(dateFormat.parse("07/01/2002").getTime()));
+            preparedStatement.setDouble(4, 5000);
+            preparedStatement.setInt(5, 1);
+
+            int rows = preparedStatement.executeUpdate();
+            if (rows > 0) {
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                while (resultSet.next()) {
+                    int id = resultSet.getInt(1);
+                    IO.println("Novo item com id " + id);
+                }
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } catch (ParseException e) {
             e.printStackTrace();
         }
         finally {
-            Db.closeResultSet(resultSet);
-            Db.closeStatement(statement);
+            Db.closeStatement(preparedStatement);
             Db.closeConnection();
         }
     }
